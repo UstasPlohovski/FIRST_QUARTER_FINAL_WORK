92d8b776233ec23f2df867dddc71f84b243955b0
Ustas <ustasplohovski@gmail.com>
2022-12-29 17:46:15 +0300

    Add files DiagramStringArray.drawio (блок-схема алгоритма) and FIRST_QUARTER_FINAL_WORK.png (фото экрана результата программы).

--- Program.cs
+++ Program.cs
@@ -1,23 +1,49 @@
-﻿
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длинна которых меньше либо равна 3 символа.
 
 Console.WriteLine("Введите размер массива");
 int Size = Convert.ToInt32(Console.ReadLine());
 string[] StringArray = new string[Size];
-for (int i = 0; i < Size; i++)
-{
-    Console.WriteLine($"Введите {i+1}-ый элемент массива");
-    StringArray[i] = Console.ReadLine();
-}
-PrintArraySort (StringArray);
 
+StringArray = FillArray(Size);
 
-void PrintArraySort (string [] Array)
+string[] ArraySort = FillArraySort(StringArray);
+
+PrintArraySort(ArraySort);
+
+// Функции:
+
+string[] FillArray(int Size)
 {
-for (int i = 0; i < Array.Length; i++)
-{
-    if (Array[i].Length <= 3)
+    string[] StringArray = new string[Size];
+    for (int i = 0; i < Size; i++)
     {
-        Console.WriteLine($"Элемент массива = {StringArray[i]}, соответствует условию сортировки");
+        Console.WriteLine($"Введите {i + 1}-ый элемент массива");
+        StringArray[i] = Console.ReadLine();
     }
+    return StringArray;
 }
+
+string[] FillArraySort(string[] Array)
+{
+    string[] ArraySort = new string[Array.Length];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+            ArraySort[i] = Array[i];
+        }
+    }
+    return ArraySort;
+}
+
+void PrintArraySort(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] != null)
+        {
+            Console.WriteLine($"Элемент массива = {Array[i]}, соответствует условию сортировки");
+        }
+    }
 }
