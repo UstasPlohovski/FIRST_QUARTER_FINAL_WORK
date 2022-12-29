f4b38d705aec931631ef856836d71a1e4bc86d2b
Ustas <ustasplohovski@gmail.com>
2022-12-29 21:44:27 +0300

    Formated file Program.cs

--- Program.cs
+++ Program.cs
@@ -8,66 +8,19 @@ int Size = Convert.ToInt32(Console.ReadLine());
 string[] StringArray = new string[Size];
 for (int i = 0; i < Size; i++)
 {
-    Console.WriteLine($"Введите {i+1}-ый элемент массива");
+    Console.WriteLine($"Введите {i + 1}-ый элемент массива");
     StringArray[i] = Console.ReadLine();
 }
-PrintArraySort (StringArray);
+PrintArraySort(StringArray);
 
 
-void PrintArraySort (string [] Array)
+void PrintArraySort(string[] Array)
 {
-for (int i = 0; i < Array.Length; i++)
-{
-    if (Array[i].Length <= 3)
+    for (int i = 0; i < Array.Length; i++)
     {
-        Console.WriteLine($"Элемент массива = {StringArray[i]}, соответствует условию сортировки");
+        if (Array[i].Length <= 3)
+        {
+            Console.WriteLine($"Элемент массива = {StringArray[i]}, соответствует условию сортировки");
+        }
     }
 }
-}
-
-// Console.WriteLine("Введите размер массива");
-// int Size = Convert.ToInt32(Console.ReadLine());
-// string[] StringArray = new string[Size];
-
-// StringArray = FillArray(Size);
-
-// string[] ArraySort = FillArraySort(StringArray);
-
-// PrintArraySort(ArraySort);
-
-// // Функции:
-
-// string[] FillArray(int Size)
-// {
-//     string[] StringArray = new string[Size];
-//     for (int i = 0; i < Size; i++)
-//     {
-//         Console.WriteLine($"Введите {i + 1}-ый элемент массива");
-//         StringArray[i] = Console.ReadLine();
-//     }
-//     return StringArray;
-// }
-
-// string[] FillArraySort(string[] Array)
-// {
-//     string[] ArraySort = new string[Array.Length];
-//     for (int i = 0; i < Array.Length; i++)
-//     {
-//         if (Array[i].Length <= 3)
-//         {
-//             ArraySort[i] = Array[i];
-//         }
-//     }
-//     return ArraySort;
-// }
-
-// void PrintArraySort(string[] Array)
-// {
-//     for (int i = 0; i < Array.Length; i++)
-//     {
-//         if (Array[i] != null)
-//         {
-//             Console.WriteLine($"Элемент массива = {Array[i]}, соответствует условию сортировки");
-//         }
-//     }
-// }
