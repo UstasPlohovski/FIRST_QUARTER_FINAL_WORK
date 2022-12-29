81731b729893e1a13e04ed9320c4eb5606f659cd
Ustas <ustasplohovski@gmail.com>
2022-12-29 16:20:33 +0300

    Add first variant task.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Введите размер массива");
+int Size = Convert.ToInt32(Console.ReadLine());
+string[] StringArray = new string[Size];
+for (int i = 0; i < Size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-ый элемент массива");
+    StringArray[i] = Console.ReadLine();
+}
+PrintArraySort (StringArray);
+
+
+void PrintArraySort (string [] Array)
+{
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length <= 3)
+    {
+        Console.WriteLine($"Элемент массива = {StringArray[i]}, соответствует условию сортировки");
+    }
+}
+}
